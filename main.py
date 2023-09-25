68653ad00ab5fa7099f844827eaff4fe20e165c3
Moeen <moeenyounis11@gmail.com>
2023-09-25 22:18:33 +0100

    `main.py`: `get_env_value` added for loading secrets

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ Project entry point; official Raspberry Pi datasheet referenced:
 https://datasheets.raspberrypi.com/picow/connecting-to-the-internet-with-pico-w.pdf
 '''
 
+import json
 from machine import Pin
 from time import sleep
 
@@ -14,5 +15,15 @@ def blink():
         sleep(0.5)
 
 
+def get_env_value(key):
+    with open('env.json', encoding='utf-8') as f:
+        return json.load(f)[key]
+
+
 if __name__ == '__main__':
-    blink()
+    ssid = get_env_value('SSID_NAME')
+    key = get_env_value('SSID_KEY')
+    if ssid and key:
+        blink()
+    else:
+        print('Network credentials not found; check `env.json`')
