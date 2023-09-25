7485024ec94537d7781d231c276ef9c82c4e08b0
Moeen <moeenyounis11@gmail.com>
2023-09-25 21:38:07 +0100

    `main.py`: LED toggling wrapped in `blink` function

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+'''
+Project entry point; official Raspberry Pi datasheet referenced:
+https://datasheets.raspberrypi.com/picow/connecting-to-the-internet-with-pico-w.pdf
+'''
 
 from machine import Pin
 from time import sleep
 
 
-if __name__ == '__main__':
+def blink():
     led = Pin('LED', Pin.OUT)
     while True:
         led.toggle()
-        sleep(0.25)
+        sleep(0.5)
+
+
+if __name__ == '__main__':
+    blink()
