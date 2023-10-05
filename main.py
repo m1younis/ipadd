b51a488e61d4de116eb73fbe677167e00b43ac6d
Moeen <moeenyounis11@gmail.com>
2023-10-05 22:14:50 +0100

    `main.py`: `initialise_lcd` added + title/datetime displayed

--- main.py
+++ main.py
@@ -2,17 +2,41 @@
 https://datasheets.raspberrypi.com/picow/connecting-to-the-internet-with-pico-w.pdf
 """
 
+import display
 import helpers
-from machine import Pin
+from lib.ili9341 import Display
+from machine import Pin, SPI
 import network
-from time import sleep
+import utime
 
 
 def blink():
     led = Pin('LED', Pin.OUT)
     while True:
         led.toggle()
-        sleep(0.5)
+        utime.sleep(0.5)
+
+
+def initialise_lcd():
+    spi = SPI(
+        0,
+        baudrate=10000000,
+        polarity=1,
+        phase=1,
+        bits=8,
+        firstbit=SPI.MSB,
+        sck=Pin(18),
+        mosi=Pin(19),
+        miso=Pin(16))
+
+    return Display(
+        spi,
+        dc=Pin(15),
+        cs=Pin(17),
+        rst=Pin(14),
+        width=320,
+        height=240,
+        rotation=90)
 
 
 if __name__ == '__main__':
@@ -24,9 +48,13 @@ if __name__ == '__main__':
         wlan.connect(ssid, key)
         if wlan.isconnected() and wlan.status() == 3:
             wlan.scan()     # Empty scan
-            print(helpers.get_salaah_meta())
-            print(helpers.get_atmospheric_meta())
-            print(helpers.get_network_meta(wlan))
+
+            lcd = initialise_lcd()
+            # Title height determines positioning of datetime and info
+            start_ypos = display.render_title(lcd)
+
+            while True:
+                display.render_datetime(lcd, start_ypos)
         else:
             print('Network connection error; check supplied credentials')
     else:
