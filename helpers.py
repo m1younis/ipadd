da0d1870ccd684d6eff88db4a4587af3f56852f9
Moeen <moeenyounis11@gmail.com>
2023-10-10 23:16:58 +0100

    `helpers.py`: `strf_sensor_val` + basic logging added

--- helpers.py
+++ helpers.py
@@ -1,11 +1,13 @@
 """Helper functions module."""
 
+from display import strf_datetime
 from lib.bme280 import BME280
 from machine import Pin, I2C
 from network import WLAN
 from ubinascii import hexlify
 import ujson
 import urequests
+import utime
 
 
 def get_env_value(key):
@@ -41,6 +43,9 @@ def get_salaah_meta():
             (meta['today']['Sunrise'], meta['today']['Maghrib']),
             (meta['today']['Dhuhr'], meta['today']['Isha\'a']))
     except Exception as e:
+        print(
+            f'({strf_datetime(*utime.localtime()[:6])})'
+            + ' [error] Primary prayer times API down')
         meta = get_raw_response(f'https://muslimsalat.com/{city}.json')['items'][0]
         meta = {
             k: v for k, v in meta.items() if k != 'date_for'
@@ -49,6 +54,12 @@ def get_salaah_meta():
             (strf_salaah_time(meta['fajr']), strf_salaah_time(meta['asr'])),
             (strf_salaah_time(meta['shurooq']), strf_salaah_time(meta['maghrib'])),
             (strf_salaah_time(meta['dhuhr']), strf_salaah_time(meta['isha'])))
+
+
+def strf_sensor_val(raw, units, prec=False):
+    val = float(raw.replace(units, ''))
+    return str(round(val) if not prec else round(val, 1)) +\
+        (units.lower if units == 'C' else units)
 
 
 def get_atmospheric_meta():
@@ -65,9 +76,9 @@ def get_atmospheric_meta():
 
     # BME280 sensor readings (internal atmosphere)
     intemp, inpres, inhumd = BME280(i2c=I2C(0, scl=Pin(1), sda=Pin(0))).values
-    intemp = f"{round(float(intemp.replace('C', '')), 1)}c"
-    inpres = f"{round(float(inpres.replace('hPa', '')))}hPa"
-    inhumd = f"{round(float(inhumd.replace('%', '')))}%"
+    intemp = strf_sensor_val(intemp, 'C', prec=True)
+    inpres = strf_sensor_val(inpres, 'hPa')
+    inhumd = strf_sensor_val(inhumd, '%')
 
     return (
         (extemp, intemp),
@@ -106,5 +117,8 @@ def get_network_meta(wlan):
                     (decode_bin_mac(wlan.config('mac')), decode_bin_mac(ap[1])),
                     (wlan.config('hostname'), get_network_auth(ap[4])),
                     ap[3])
-
-    return ()
+    else:
+        print(
+            f'({strf_datetime(*utime.localtime()[:6])})'
+            + ' [error] Network connection interrupted; status code: {wlan.status()}')
+        return ()
